78010c11aa6ee4385aec414ae2d762757189844e
aziz42_ux <azizisoffline@gmail.com>
2021-12-17 21:15:41 +0500

    Socail Accounts Login React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import GoogleLogin from "react-google-login";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [userData, setUserData] = useState([]);
+
+  const responseGoogle = (response) => {
+    let index = userData.findIndex(
+      (e) => e.email === response.profileObj.email
+    );
+    if (index !== -1) {
+      alert("this email is already registered");
+    } else {
+      setUserData([
+        ...userData,
+        {
+          name: response.profileObj.name,
+          email: response.profileObj.email,
+        },
+      ]);
+    }
+  };
+  const onDelete = (item) => {
+    setUserData(
+      userData.filter((e) => {
+        return e.email !== item;
+      })
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid text-center">
+      <h2>Google Login</h2>
+      <GoogleLogin
+        clientId="Your Client Id Goes Here"
+        buttonText="Sign in with Google"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      {userData.map((items, index) => {
+        return (
+          <div
+            className="d-flex"
+            key={index}
+            style={{
+              boxShadow: "0 3px 10px rgb(0 0 0 / 0.2)",
+              height: "50px",
+              width: "300px",
+              marginLeft: "38%",
+              marginTop: "20px",
+            }}
+          >
+            <div>
+              <button
+                className="btn btn-danger btn-sm my-2"
+                onClick={() => {
+                  onDelete(items.email);
+                }}
+              >
+                X
+              </button>
+            </div>
+            <div style={{ marginLeft: "20px" }}>
+              {items.name}
+              <br></br>
+              {items.email}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
